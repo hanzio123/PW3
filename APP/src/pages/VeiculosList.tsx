17ade74b41d190a4c51feb1cbcbaad40ecbf2934
Vladimir Makarov <liqmjp513@gmail.com>
2026-04-09 10:52:16 +0000

    FinalAula_06

--- APP/src/pages/VeiculosList.tsx
+++ APP/src/pages/VeiculosList.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { getVeiculos } from "../services/veiculoService";
 import type { Veiculo } from "../types/veiculo";
+import { VeiculoCard } from "../components/VeiculoCard";
 
 export function VeiculosList() {
   const [veiculos, setVeiculos] = useState<Veiculo[]>([]);
@@ -14,12 +15,8 @@ export function VeiculosList() {
     <div>
       <h1>Lista de Veículos</h1>
 
-      {veiculos.map((veiculo) => (
-        <div key={veiculo.id}>
-          <h2>{veiculo.modelo}</h2>
-          <p>{veiculo.descricao}</p>
-          <strong>R$ {veiculo.valor}</strong>
-        </div>
+      {veiculos.map((V) => (
+        <VeiculoCard key={V.id} veiculo={V} />
       ))}
     </div>
   );
